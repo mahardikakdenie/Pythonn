ad012222dfcf809306a1493c17e0f2ffa5dcfa8f
mahardika denie <dikamahar884@gmail.com>
2020-12-25 20:51:29 +0700

    Konversi SUHU

--- 04-InputCommand/src/main2.py
+++ 04-InputCommand/src/main2.py
@@ -1,1 +1,18 @@
-print("hello")
+# Operator Hitung Program Koversi Temperatur
+
+print("\nProgram Konversi Temperatur\n")
+
+Celcius = float(input("Masukan Suhu dalam Celcius : "))
+print("Suhu dalam Celcius adalah ",Celcius,"Celcius")
+
+#reamur
+reamur =  (4 / 5) * Celcius
+print("Suhu dalam reamur adalah ",reamur,"Reamur")
+
+# fahrenheit
+fahrenheit = (( 9 / 5) * Celcius) + 32
+print("Suhu dalam Fahrenheit adalah ",fahrenheit,"Fahrenheit")
+
+# Kelvin
+Kelvin = Celcius + 273
+print("Suhu dalam Kelvin adalah = ",Kelvin,"Kelvin")
